c6db35187c07a39f70cd7088b161e8a70cc5fc52
dnakano <dnakano@student.42tokyo.jp>
2021-07-03 11:34:11 +0900

    Add Dashboard (22:00)
    22:00 on this video
    https://www.youtube.com/watch?v=hiiaHyhhwBU

---
+++ src/Dashboard.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Paper, Theme } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    margin: "50px",
+    padding: theme.spacing(3, 2),
+  },
+  flex: {
+    display: "flex",
+    alignItems: "center",
+  },
+  topicsWindow: {
+    width: "30%",
+    height: "300px",
+    borderRight: "1px solid gray",
+  },
+  chatWindow: {
+    width: "70%",
+    height: "300px",
+    padding: "20px",
+  },
+  chatBox: {
+    width: "85%",
+  },
+  button: {
+    width: "15%",
+  },
+  chip: {},
+  textField: {},
+}));
+
+export default function Dashboard() {
+  const classes = useStyles();
+
+  const [textValue, changeTextValue] = React.useState("")
+
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <h1>Chat app</h1>
+        <p>Topic Placeholder</p>
+        <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
+            <List>
+              {["topic"].map((topic) => (
+                <ListItem key={topic} button>
+                  <ListItemText primary={topic} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {[{ from: "user", msg: "hello" }].map((chat, i) => (
+              <div className={classes.flex} key={i}>
+                <Chip label={chat.from} className={classes.chip} />
+                <p>{chat.msg}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={classes.flex}>
+          <TextField
+            className={classes.chatBox}
+            label="Send a chat"
+            value={textValue}
+            onChange={(event) => changeTextValue(event.target.value)}
+          />
+          <Button variant="contained" color="primary">
+            Send
+          </Button>
+        </div>
+      </Paper>
+          <p>{textValue}</p>
+    </div>
+  );
+}
